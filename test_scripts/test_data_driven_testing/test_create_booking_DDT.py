009c240de2f4f649cb0f7d9fb04804e7d44b5e5e
ravilohana <ravi.lohana1987@gmail.com>
2023-08-02 08:50:55 +0530

    add comments to test_create_booking_DDT

--- test_scripts/test_data_driven_testing/test_create_booking_DDT.py
+++ test_scripts/test_data_driven_testing/test_create_booking_DDT.py
@@ -1,4 +1,4 @@
-# restful-booker --- Create booking (POST)
+
 from src.constants.api_constants import url_create_booking
 from src.helpers.api_wrapper import post_request
 from src.helpers.common_verification import verify_http_code, verify_key
@@ -9,6 +9,10 @@ import pprint
 
 class Test_Create_Booking_DDT(object):
 
+    # restful-booker --- Create booking (POST)
+    # Data Driven testing using JSON file
+    #  path : src/resources/data_create_booking_ddt.json
+    
     def test_create_booking_ddt(self):
         response = post_request(url=url_create_booking(),
                                 headers=common_headers(),
